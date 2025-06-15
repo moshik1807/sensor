f5bacf0ea3bce7bcb2bb1c5360acb64304ad7832
moshik <moshik0527669169@gmail.com>
2025-06-15 13:06:12 +0300

    add sensors

---
+++ sensor/Sensors/sensorModel.cs
@@ -0,0 +1,14 @@
+﻿namespace sensor.models
+{
+    public abstract class Sensor
+    {
+        public string Name;
+        public Sensor(string name)
+        {
+            Name = name;
+        }
+        public abstract void Activate();
+     
+    }
+}
+
