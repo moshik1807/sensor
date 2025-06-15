fd6353223396e7bec0cc2d26a1f872174448182c
moshik <moshik0527669169@gmail.com>
2025-06-15 13:08:07 +0300

    add menu,terorrist,and program

---
+++ sensor/Menu.cs
@@ -0,0 +1,31 @@
+﻿namespace sensor.models
+{
+    public class Menu
+    {
+
+        public Menu()
+        {
+            Audio audio = new Audio("ss");
+            Thermal thermal = new Thermal("tt");
+            audio.Activate();
+            audio.Activate();
+            Console.WriteLine(audio.Name);
+            Terorrist n = new Terorrist("n");
+            Console.WriteLine(n.Checked(audio));
+            
+        }
+        public void Game()
+        {
+            Console.WriteLine("ENTER");
+            string coice = Console.ReadLine();
+            switch (coice)
+            {
+                case "a":
+                    break;
+            }
+               
+        }
+
+    }
+}
+
---
+++ sensor/terorrist.cs
@@ -0,0 +1,20 @@
+﻿namespace sensor.models
+{
+    public class Terorrist
+    {
+        public List<string> SensorsActivated = new List<string>();
+        public List<string> SuitableSensors = new List<string>();
+        string Name;
+        public Terorrist(string name)
+        {
+            Name = name;
+        }
+        public bool Checked(Sensor sensor)
+        {
+            return (SuitableSensors.Contains(sensor.Name));
+        }
+            
+
+    }
+}
+
